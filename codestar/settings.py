e72d0e580d9a4b562667bb4dc13db1bdf0a55140
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-01-30 17:23:12 +0000

    Update project for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q9)q7n_8wsqo1b13#m=^xzh+j8bx!em(v#48m4^j58sac)(oo^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
