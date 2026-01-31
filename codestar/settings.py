c32f578e16e58c353104b968f734abd92de91c89
hamza-m1 <177459013+hamza-m1@users.noreply.github.com>
2026-01-31 00:29:20 +0000

    Replace hardcoded SECRET_KEY with environment variable for security

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q9)q7n_8wsqo1b13#m=^xzh+j8bx!em(v#48m4^j58sac)(oo^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
